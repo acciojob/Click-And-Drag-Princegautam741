5413d50eb70b0be328f6a818e7952f89385c0814
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-23 14:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// Your code here.
+const items = document.querySelector('.items');
+
+let isDragging = false;
+let startPosition = 0;
+let scrollLeft = 0;
+
+items.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startPosition = e.pageX - items.offsetLeft;
+  scrollLeft = items.scrollLeft;
+  items.classList.add('active');
+});
+
+items.addEventListener('mouseleave', () => {
+  isDragging = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mouseup', () => {
+  isDragging = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  const x = e.pageX - items.offsetLeft;
+  const walk = (x - startPosition) * 3; // Adjust the sensitivity
+  items.scrollLeft = scrollLeft - walk;
+});
+
+items.addEventListener('click', (e) => {
+  if (isDragging) {
+    e.preventDefault();
+  }
+});
